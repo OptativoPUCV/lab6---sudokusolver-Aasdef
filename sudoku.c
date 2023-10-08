00fae106d410049c940412c385f0979c2bc97d7e
Replit user <>
2023-10-08 00:03:47 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,23 +44,73 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-
-    return 1;
+  //No se repitan números en las filas
+  for (int i=0;i<0; i++){
+    int aux[10]={0};
+    for (int j=0;j<9;j++){
+      int numero=n->sudo[i][j];
+      if (numero<1||numero>9||aux[numero]==1){
+        return 0;
+      }
+      aux[numero]=1;
+    }
+  }
+  //No se repitan números en las columnas
+  for(int j=0;j<9;j++){
+    int aux[10]={0};
+    for (int i=0;i<9;i++){
+      int numero =n->sudo[i][j];
+      if (numero <1 || numero>9 || aux[numero]==1){
+        return 0;
+      }
+      aux[numero]=1;
+    }  
+  }
+  //No se repitan números en las submatrices de 3x3
+  for(int fila =0; fila <9; fila +=3){
+    for (int columna=0; columna <9; columna +=3){
+      int usado [10]={0};
+      for(int i=fila ; i<fila +3; i++){
+        for (int j=columna ; j<columna ;j++){
+          int numero =n->sudo[i][j];
+          if (numero <1||numero >9 || usado[numero]==1){
+            return 0;
+          }
+          usado[numero]=1;
+        }
+      }
+    }
+  }
+  return 1;
 }
 
 
 List* get_adj_nodes(Node* n){
   //Cree una función que a partir de un nodo genere una lista con los nodos adyacentes
   List* list=createList();
-  //recorrer matriz de 9x9
-  for(int i=0;i<9;i++){
-    for (int j=0; j<9;j++){
-      //si esta vacía se llena con un numero
-      if(n->sudo[i][j]==0){
-        Node* nuevo=createNode();
-        memcpy(nuevo->sudo, n->sudo, 9*9*sizeof(int));
+
+  int filaVacia=-1;
+  int coluVacia=-1;
+  
+  for (int i=0; i<9 && filaVacia==-1;i++){
+    for (int j=0;i<9 &&coluVacia==-1;j++){
+      if (n->sudo[i][j]==0){
+        filaVacia=i;
+        coluVacia=j;
+        break;
         
+      }
+    }
+  }
+
+  if (filaVacia!=-1 &&coluVacia==-1){
+    //buscar numeros vacios 
+    for (int numero=1; numero<=9;numero++){
+      Node*nuevo=copy(n);
+      if(is_valid(nuevo)){
         pushBack(list, nuevo);
+      }else{
+        free(nuevo);
       }
     }
   }
